20c0e7a1ff3caead04de4c94d6cdb9655e3d8a4e
Hris <xrisko06@abv.bg>
2024-03-30 21:18:19 +0200

    update register dto

--- RentNRoll.Web.DTOs/Register/RegisterDTO.cs
+++ RentNRoll.Web.DTOs/Register/RegisterDTO.cs
@@ -1,13 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AutoMapper;
+using RentNRoll.Data.Models;
+using RentNRoll.Services.Mapping;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentNRoll.Web.DTOs.Register
 {
-	public class RegisterDTO
+	public class RegisterDTO : IMapTo<ApplicationUser>, IHaveCustomMappings
 	{
+		public required string FirstName { get; set; }
+		public required string LastName { get; set; }
 		public required string Username { get; set; }
 		public required string Password { get; set; }
 
 		[EmailAddress]
 		public required string Email { get; set; }
+		public required string Phone { get; set; }
+		public required string EGN { get; set; }
+
+		public void CreateMappings(IProfileExpression configuration)
+		{
+			configuration.CreateMap<RegisterDTO, ApplicationUser>()
+				.ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+				.ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Phone));
+		}
 	}
 }
