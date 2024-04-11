87d2ec4eb389764f6c019529aeb281b50f8c8c90
Hris <xrisko06@abv.bg>
2024-04-11 21:01:50 +0300

    add brand and category service on front end

--- clientapp/src/app/components/cars/cars.component.ts
+++ clientapp/src/app/components/cars/cars.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { BrandService } from '../../services/brand/brand.service';
+import { CategoryService } from '../../services/category/category.service';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit {
+  brands: string[] = [];
+  categories: string[] = [];
 
+  constructor(private brandService: BrandService, private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.brandService.getBrands().subscribe(data => this.brands = data);
+    this.categoryService.getCategories().subscribe(data => this.categories = data);
+  }
 }
